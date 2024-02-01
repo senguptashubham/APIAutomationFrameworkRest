d018c0c943569fd9ec213601a94e050bc5a7475b
shubhamsengupta <ishubham.sg@gmail.com>
2024-02-01 20:32:27 +0530

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace APIAutomationFrameworkRest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
